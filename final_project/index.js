8a24ed41b5598d8e7c2d96ef8a05df7fb17bde6f
mohamedfarhan7 <140420899+mohamedfarhan7@users.noreply.github.com>
2023-09-08 17:47:05 +0530

    final project completed

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.session.authorization["accessToken"];
+    if (token) {
+        jwt.verify(token, "access", (err, user) => {
+            if (err)
+                return res.status(401).json({"message": "Unauthorized"});
+
+            req.user = user;
+            next();
+        });
+    } else {
+        return res.status(400).json({"message": "User not logged in"});
+    }
 });
  
 const PORT =5000;
